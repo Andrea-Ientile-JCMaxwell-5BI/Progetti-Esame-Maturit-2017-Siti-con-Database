af31d1e1514e2e5285a3aa3eb10f374fc7c57f76
AnnabbellaLaBaldraccaPUTTANELLAKISSKISSLOVE <andrea.ientile@jcmaxwell.it>
2017-03-02 14:32:34 +0100

    Update process2.php

--- process2.php
+++ process2.php
@@ -13,19 +13,19 @@
 
  $email = stripcslashes($email);
  $password = stripcslashes($password);
- $email = mysql_real_escape_string($email);
- $password = mysql_real_escape_string($password);
+ $email = mysqli_real_escape_string($email);
+ $password = mysqli_real_escape_string($password);
 
 //connessione al server e selezione Database
 
-mysql_connect("localhost", "root","");
-mysql_select_db("login");
+$conn = mysqli_connect("localhost","root","");
+$sel = mysqli_select_db($conn, "login");
 
 // Query  al Database da parte dell'utente
 
-$result = mysql_query("SELECT * FROM users WHERE email = '$email' and password = '$password'")
+$result = mysqli_query("SELECT * FROM users WHERE email = '$email' and password = '$password'")
             or die("Failed to query database ".mysql_error());
-$row = mysql_fetch_array($result);
+$row = mysqli_fetch_array($result);
 if ($row['email'] == $email && $row['password'] == $password ){
  
   echo "Login avvenuto con successo! Benvenuto.".$row['username];
