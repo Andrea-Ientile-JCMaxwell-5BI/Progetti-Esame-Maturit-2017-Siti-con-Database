cbb3d317f9e8172e08c1f2860ead38dde17afce0
AnnabbellaLaBaldraccaPUTTANELLAKISSKISSLOVE <andrea.ientile@jcmaxwell.it>
2017-03-08 11:52:46 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,22 +1,35 @@
 <html>
 	<head>
 		<title>Login Page</title>
+		 <link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<div>
-			<form action= "process.php" method="POST">
-				<p>
-					<label> Username </label>
-					<input type = "text" id = "User" name = "User"/>
-				</p>
-				<p>
-					<label> Password </label>
-					<input type = "password" id = "Pass" name = "Pass"/>
-				</p>
-				<p>
-					<input type = "submit" id = "btn" value = "Login"/>
-				</p>
+		
+			<form action= "process.php" method="POST" >
+				<table id=frm align="center">
+				
+				<tr>
+					<td><label> Username </label></td>
+					<td><input type = "text" id = "User" name = "User" align="center"/></td>
+				
+				</tr>
+				<tr>
+					<td><label> Password </label></td>
+					<td><input type = "password" id = "Pass" name = "Pass" align="center"/></td>
+					 
+				</tr>
+				<tr>
+
+				 <td></td>
+	             <td align="center"> <input type = "submit" id = "btn" value = "Login"/> </td>
+				</tr>
+				</table>
+				
+					
+				
+				
+				
 			</form>
-		</div>
+		
 	</body>
 </html>
