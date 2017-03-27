1d47606644c50e02bfeb1ba66724d5151344c07b
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-03-27 09:45:29 +0200

    Update Classi.php

--- Classi.php
+++ Classi.php
@@ -1,20 +1,12 @@
+<!DOCTYPE html>
 <html>
-
-  <head>
-  <title>Classi</title>
-  </head>
-
 <body>
 
+<form action="/action_page.php" method="get">
+  <input type="checkbox" name="vehicle" value="Bike"> I have a bike<br>
+  <input type="checkbox" name="vehicle" value="Car" checked="checked"> I have a car<br>
+  <input type="submit" value="Submit">
+</form>
 
-<label> Classi </label>
-
-
-</body>  
-  
-  
-
-
-
-
+</body>
 </html>
