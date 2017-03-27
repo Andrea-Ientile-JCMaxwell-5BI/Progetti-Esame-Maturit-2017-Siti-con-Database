7d8cc6eb8d87bf3d7e9fd27b668379785517a5d5
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-03-27 08:33:08 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -9,13 +9,11 @@
 				<table width= "450" id=frm align="center">
 				
 				<tr>
-					<td align="center" width="50%"><label> Username </label></td>
-					<td width="50%"><input type = "text" id = "User" name = "User" align="center"/></td>
+					<td align="center" width="50%"><input type = "text" id = "User" name = "User" placeholder="Username"/></td>
 				
 				</tr>
 				<tr>
-					<td align="center" width="50%"><label> Password </label></td>
-					<td width="50%"><input type = "password" id = "Pass" name = "Pass" align="center"/></td>
+				<td align="center" width="50%"><input type = "password" id = "Pass" name = "Pass" placeholder="Password"/></td>
 					 
 				</tr>
 				<tr align="center">
