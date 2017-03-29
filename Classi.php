edec0e920b9ef6e6e997ce8684d87783d3e15b5e
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-03-29 11:50:54 +0200

    Update Classi.php

--- Classi.php
+++ Classi.php
@@ -1,12 +1,66 @@
 <!DOCTYPE html>
 <html>
-<body>
+<?php
 
-<form action="/action_page.php" method="get">
-  <input type="checkbox" name="vehicle" value="Bike"> I have a bike<br>
-  <input type="checkbox" name="vehicle" value="Car" checked="checked"> I have a car<br>
-  <input type="submit" value="Submit">
-</form>
+if(isset($_POST['search']))
+{
+    $valueToSearch = $_POST['valueToSearch'];
+    // search in all table columns
+    // using concat mysql function
+    $query = "SELECT * FROM `classi` WHERE CONCAT(`id_classe`, `aula`, `n_alunni`) LIKE '%".$valueToSearch."%'";
+    $search_result = filterTable($query);
+    
+}
+ else {
+    $query = "SELECT * FROM `classi`";
+    $search_result = filterTable($query);
+}
 
-</body>
+// function to connect and execute the query
+function filterTable($query)
+{
+    $connect = mysqli_connect("localhost", "root", "", "database_sito");
+    $filter_Result = mysqli_query($connect, $query);
+    return $filter_Result;
+}
+
+?>
+
+
+
+    <head>
+        <title>PHP HTML TABLE DATA SEARCH</title>
+        <style>
+            table,tr,th,td
+            {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <form action="Classi.php" method="post">
+            <input type="text" name="valueToSearch" placeholder="Value To Search"><br><br>
+            <input type="submit" name="search" value="Filter"><br><br>
+            
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                   
+                </tr>
+				
+				 <?php while($row = mysqli_fetch_array($search_result)):?>
+                <tr>
+                    <td><?php echo $row['id_classe'];?></td>
+                    <td><?php echo $row['aula'];?></td>
+                    <td><?php echo $row['n_alunni'];?></td>
+            
+                </tr>
+                <?php endwhile;?>
+            </table>
+        </form>
+        
+    </body>
 </html>
