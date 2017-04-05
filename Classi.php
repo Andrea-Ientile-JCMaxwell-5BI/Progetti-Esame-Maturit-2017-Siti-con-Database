1a1c2df9f1d6b2a78ab7a90f7528dc49445c1ac0
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-04-05 11:42:06 +0200

    Update Classi.php

--- Classi.php
+++ Classi.php
@@ -24,6 +24,27 @@ function filterTable($query)
     return $filter_Result;
 }
 
+if(isset($_POST['visualizza'])) //PULSATE PER VISUALIZZARE ALUNNI
+{
+    $valore_da_cercare = $_POST['valore_da_cercare'];
+    // search in all table columns
+    // using concat mysql function
+    $query1 = "SELECT alunni.* FROM `alunni,classi` WHERE CONCAT(`aula`) LIKE '%".$valore_da_cercare."%' AND alunni.id_alunni = classi.id_classe";
+    $search_result1 = filterTable1($query1);
+    
+}
+ else {
+  
+}
+
+// function to connect and execute the query
+function filterTable1($query1)
+{
+    $connect = mysqli_connect("localhost", "root", "", "database_sito");
+    $filter_Result1 = mysqli_query($connect, $query1);
+    return $filter_Result1;
+}
+
 ?>
 
 
@@ -43,6 +64,7 @@ function filterTable($query)
                     <th>Id Classe</th>
                     <th>Aula</th>
                     <th>Numero Alunni</th>
+	            <th>Visualizza Alunni</th>
                    
                 </tr>
 				
@@ -51,12 +73,13 @@ function filterTable($query)
                     <td><?php echo $row['id_classe'];?></td>
                     <td><?php echo $row['aula'];?></td>
                     <td><?php echo $row['n_alunni'];?></td>
+	            <td> <input type = "submit" name="visualizza" id = "btn3" value ="visualizza"/> </td>
             
                 </tr>
                 <?php endwhile;?>
             </table>
 			
-			<p align="center"> <input class="txt" type="text" name="valore_da_cercare" placeholder="Classe..."><br><br>
+				<p align="center"> <input class="txt" type="text" name="valore_da_cercare" placeholder="Classe..."><br><br>
                        <input class="btn" type="submit" name="search" value="Cerca per classe">
 					   <input class="btn1" type="submit" name="search" value="Reset">
 					   </p>
