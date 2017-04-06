d341390f9e63df84b6d154d99060ca580c54d514
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-04-06 21:44:59 +0200

    Update registrazione.php

--- registrazione.php
+++ registrazione.php
@@ -7,7 +7,7 @@
 		
 		<div class="background-wrap">
 			<video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> 
-				<source src="videoplayback.mp4" type="video/mp4">
+				<source src="video_registrazione.mp4" type="video/mp4">
 				Video not supported
 			</video>          
 		</div>
