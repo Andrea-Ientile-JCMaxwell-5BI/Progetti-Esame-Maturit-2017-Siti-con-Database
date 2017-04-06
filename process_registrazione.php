74e60e4abfc9dcd3160e464b129d9557fc36acfd
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-04-06 15:41:16 +0200

    Add files via upload

---
+++ process_registrazione.php
@@ -0,0 +1,38 @@
+<?php
+//la stringa mysql_connect deve essere compilata con i dati relativi al proprio database
+
+
+$link = mysqli_connect("localhost","root","");//database connection
+//nome DB
+mysqli_select_db($link,"database_sito");
+
+// recupero i valori si NOME e INDIRIZZO e li assegno alle variabili $name e $address
+$Username = $_POST['username'];
+$Password = $_POST['password'];
+$Nome = $_POST['nome'];
+$Cognome = $_POST['cognome'];
+$Data_nascita = $_POST['data_nascita'];
+
+
+
+//inserting data order
+$toinsert = "INSERT INTO professori
+			(nome,cognome,data_nascita,username,password)
+			VALUES
+			('$Nome',
+			'$Cognome',
+			'$Data_nascita',
+			'$Username',
+			 '$Password')";
+
+//declare in the order variable
+$result = mysqli_query($link,$toinsert) or die("Problema nella query \"$toinsert\": " .mysqli_error($link));  	//order executes
+if($result){
+	echo("<br>Inserimento avvenuto correttamente, attendere reindirizzamento alla pagina di Login...");
+	 header( "refresh:4;url=login.php" );
+	 exit;
+} else{
+	echo("<br>Inserimento non eseguito, riprovare");
+	header( "refresh:5;url=registrazione.html" );
+}
+?>
---
+++ registrazione.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<title>Pagina Registrazione</title>
+		 <link rel="stylesheet" type="text/css" href="style_login.css">
+	</head>
+	<body id="body">
+		
+		<div class="background-wrap">
+			<video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> 
+				<source src="videoplayback.mp4" type="video/mp4">
+				Video not supported
+			</video>          
+		</div>
+		
+			<form action= "process_registrazione.php" method="POST" >
+				<table width= "450" id=frm align="center">
+				
+				<tr>
+					<td align="center" width="50%"><input type = "text" id = "nome" name = "nome" placeholder="Nome..."/></td>
+				
+				</tr>
+				<tr>
+				<td align="center" width="50%"><input type = "text" id = "cognome" name = "cognome" placeholder="Cognome..."/></td>
+					 
+				</tr>
+				
+				<tr>
+					<td align="center" width="50%"><input type = "text" id = "data_nascita" name = "data_nascita" placeholder="Data di nascita..."/></td>
+				
+				</tr>
+				<tr>
+				<td align="center" width="50%"><input type = "text" id = "username" name = "username" placeholder="Username..."/></td>
+					 
+				</tr>
+				
+				<tr>
+				<td align="center" width="50%"><input type = "password" id = "password" name = "password" placeholder="Password..."/></td>
+					 
+				</tr>
+				
+				<tr align="center">
+
+				 
+	             <td colspan="2" width="30%">
+				 <input type = "submit" id = "btn" value = "Registrati"/>
+				 <input type = "reset" id = "btn2" value = "Reset"/>
+				 
+				 </td>
+				 
+				</tr>
+				</table>
+				
+					
+				
+				
+				
+			</form>
+		
+	</body>
+</html>
