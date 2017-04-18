486fa5f61f508f23e5340a846568925ca859fe6a
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-04-18 22:32:04 +0200

    Add files via upload

---
+++ first.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+  <title>Pagina Iniziale</title>
+  <link rel="stylesheet" type="text/css" href="style_first.css">
+</head>
+<body>
+  <div class="background-wrap">
+    <video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
+      <source src="video_registrazione.mp4" type="video/mp4">
+      Video not supported
+    </video>
+  <table width= "450" id=frm align="center">
+
+
+  <tr align="center">
+
+
+         <td colspan="2" width="50%">
+           <button name="primo" id="primo" onclick="location.href='Login.php'">Login</button>
+
+
+   </td>
+
+  </tr>
+  <tr align="center">
+
+
+         <td colspan="2" width="50%">
+
+           <button name="secondo" id="secondo" onclick="location.href='registrazione.php'">Registrazione</button>
+
+   </td>
+
+  </tr>
+  </table>
+
+
+
+
+</body>
+</html>
