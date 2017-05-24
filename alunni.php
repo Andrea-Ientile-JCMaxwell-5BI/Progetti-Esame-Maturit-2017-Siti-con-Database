53f9ca2297446bda088bf7a54d512652101fd7a5
Andrea-Ientile-5BI <andrea.ientile@jcmaxwell.it>
2017-05-24 11:06:40 +0200

    Modifica funzioni Alunni
    invece della ricerca per cognome ho deciso di poter cercare per classe. cosi' da visualizzare solo gli alunni di una determinata classe.

--- alunni.php
+++ alunni.php
@@ -21,12 +21,12 @@ if(isset($_POST['search']))
     $valore_da_cercare = $_POST['valore_da_cercare'];
     // search in all table columns
     // using concat mysql function
-    $query = "SELECT alunni.* FROM gestiscono, professori, alunni WHERE gestiscono.id_professore = professori.id_professore AND gestiscono.id_alunno = alunni.id_alunno AND professori.username ='$username' AND alunni.cognome LIKE '%".$valore_da_cercare."%'";
+    $query = "SELECT alunni.*, classi.aula FROM gestiscono, professori, alunni, classi WHERE gestiscono.id_professore = professori.id_professore AND gestiscono.id_alunno = alunni.id_alunno AND professori.username ='$username' AND alunni.id_classe = classi.id_classe AND classi.aula LIKE '%".$valore_da_cercare."%'";
     $search_result = filterTable($query);
 
 }
  else {
-   $query = "SELECT alunni.* FROM gestiscono, professori, alunni WHERE gestiscono.id_professore = professori.id_professore AND gestiscono.id_alunno = alunni.id_alunno  AND professori.username ='$username';";
+   $query = "SELECT alunni.*, classi.aula FROM gestiscono, professori, alunni, classi WHERE gestiscono.id_professore = professori.id_professore AND gestiscono.id_alunno = alunni.id_alunno  AND professori.username ='$username' AND alunni.id_classe = classi.id_classe;";
    $search_result = filterTable($query);
 }
 
@@ -59,6 +59,7 @@ function filterTable($query)
                     <th>Cognome</th>
                     <th>Data di nascita</th>
                     <th>Codice fiscale</th>
+					<th>Classe</th>
 
 
                 </tr>
@@ -69,13 +70,14 @@ function filterTable($query)
                     <td><?php echo $row['cognome'];?></td>
                     <td><?php echo $row['data_nascita'];?></td>
                     <td><?php echo $row['codice_fiscale'];?></td>
+					<td><?php echo $row['aula'];?></td>
 
 
                 </tr>
                 <?php endwhile;?>
             </table>
 
-				<p align="center"> <input class="txt" type="text" name="valore_da_cercare" placeholder="Cognome..."><br><br>
+				<p align="center"> <input class="txt" type="text" name="valore_da_cercare" placeholder="Classe..."><br><br>
                        <input class="btn" type="submit" name="search" value="Cerca per cognome">
 					   <input class="btn1" type="submit" name="search" value="Reset">
 					   </p>
